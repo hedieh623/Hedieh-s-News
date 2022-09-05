924d272942c9759163cf8bd8d2eb612e6af0cb2b
Hedieh <hedieh6@gmail.com>
2022-09-05 16:35:45 +0100

    Task 3- read topics in the database and returning it in the controller

--- Controllers/Topic.controller.js
+++ Controllers/Topic.controller.js
@@ -2,7 +2,7 @@ const selectTopics = require("../Models/Topic.model.js");
 
 const getTopics = (req, res) => {
   selectTopics().then((topics) => {
-    res.status(200).send("topic");
+    res.status(200).send(topics);
   });
 };
 
