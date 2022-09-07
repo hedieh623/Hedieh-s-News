cc7d180aa3309f712f1c44e5e1b147e91a6a3707
Hedieh <hedieh6@gmail.com>
2022-09-07 15:26:53 +0100

    added error handling to users controller

--- Controllers/Users.controller.js
+++ Controllers/Users.controller.js
@@ -1,10 +1,15 @@
 const selectUsers = require("../Models/Users.model.js");
 
-const getUsers = (req, res) => {
-  selectUsers().then((users) => {
-        res.status(200).send(users);
-
-  });
+const getUsers = (req, res,next) => {
+  selectUsers()
+  .then((users) => {
+    res.status(200).send(users);
+  })
+   .catch((error)=>{
+    next(error)
+  })
+  //we use error handlers so that we show the user a helpful message about what went wrong and to stop the server from dying.
+ // it works by saying let me select the users,if i fail ill let the next handle it
 };
 
 module.exports = { getUsers };
