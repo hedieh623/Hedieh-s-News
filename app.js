12a26e345bc96160f0a32aadf8bb55a56db206a3
Hedieh <hedieh6@gmail.com>
2022-09-07 12:41:15 +0100

    now closing app after tests

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const {getTopics} = require("./Controllers/Topic.controller.js");
 const app = express();
 app.get("/api/topics", getTopics);
 
-app.listen(8080)
+const server = app.listen(8080)
 
-module.exports = app;
+module.exports = server;
