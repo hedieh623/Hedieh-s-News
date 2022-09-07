5d8d9d079572a6f45da0ac8a09c80331ed265c82
Hedieh <hedieh6@gmail.com>
2022-09-07 12:45:13 +0100

    remove unnecessary console.log

--- Models/Users.model.js
+++ Models/Users.model.js
@@ -1,6 +1,5 @@
 const db = require("../db/connection.js");
 const selectUsers = () => {
-    console.log("query in db")
   return db.query("SELECT * FROM users;").then((result) => {
     return result.rows;
   });
