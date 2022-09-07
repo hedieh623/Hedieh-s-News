110530c979dd844b8ac3b45a269807056caa90de
Hedieh <hedieh6@gmail.com>
2022-09-07 15:31:45 +0100

    added error handling for articles endpoint

--- Controllers/Article.controller.js
+++ Controllers/Article.controller.js
@@ -1,13 +1,19 @@
 const selectArticle = require("../Models/Article.model.js");
 
 const getArticle = (req, res) => {
-  selectArticle(req.params.article_id).then((article) => {
-    if(article){
+  selectArticle(req.params.article_id)
+    .then((article) => {
+      if (article) {
         res.status(200).send(article);
-    } else{
-        res.status(404).send(`No article was found with id: ${req.params.article_id}`)
-    }
-  });
+      } else {
+        res
+          .status(404)
+          .send(`No article was found with id: ${req.params.article_id}`);
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 
