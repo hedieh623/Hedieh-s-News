71463c24a46b48707343ce4cecb430d195f0c44d
Hedieh <hedieh6@gmail.com>
2022-09-08 12:44:08 +0100

    remove comments

--- Controllers/Users.controller.js
+++ Controllers/Users.controller.js
@@ -8,8 +8,6 @@ const getUsers = (req, res,next) => {
    .catch((error)=>{
     next(error)
   })
-  //we use error handlers so that we show the user a helpful message about what went wrong and to stop the server from dying.
- // it works by saying let me select the users,if i fail ill let the next handle it
 };
 
 module.exports = { getUsers };
